10b8713437d3354f44390075865a1fca38edee7e
julipereiran-hub <julipereiran@gmail.com>
2025-07-22 21:56:47 -0300

    Add .next/static/chunks/src_app_page_tsx_9148afbf._.js 1753232233342

---
+++ .next/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_92d586d2._.js",
+  "static/chunks/node_modules_a0e0872c._.js"
+],
+    source: "dynamic"
+});
